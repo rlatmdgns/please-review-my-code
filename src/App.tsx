0e3bd5f2b0ef899e9fb688e1f1f46e8b7c2856aa
김승훈 <rlatmdgns94@naver.com>
2022-06-18 21:11:04 +0900

    refactor: 폴더 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CreateReview from './pages/CreateReview';
+import CreateReview from './domain/CreateReview';
 
 function App() {
   return (
