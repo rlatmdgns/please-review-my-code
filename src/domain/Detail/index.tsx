63534ae7f4a648585a5ec10238302db16ef433ff
hyeongjun94 <whow1101@naver.com>
2022-06-19 20:56:10 +0900

    feature: detail, data 연동

--- src/domain/Detail/index.tsx
+++ src/domain/Detail/index.tsx
@@ -5,34 +5,50 @@ import CodeBlock from './CodeBlock';
 import CodeComment from './CodeComment';
 import Comment from './Comment';
 import * as Style from './style';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useLayoutEffect } from 'react';
 
-function getPostData(id: number): Promise<Post> {
-  const dummyData: Post[] = [
-    {
-      title: '타이틀타이틀',
-      content: '<p>jjljkljljkljljkljkljlkjkljㅌㅋㅌㅇㅁㄴㅇㅁ</p>',
-      code: 'function test(){\n    console.log(1)\n}\n',
-      tag: ['tag1', 'tag2'],
-      category: '디버깅',
-      date: '2020-03-14',
-    },
+import { db } from 'utils/firebase/db/db';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
 
-    {
-      title: '타이틀타이틀2',
-      content: '<p>jjljkljljkljljkljkljlkjkljㅌㅋㅌㅇㅁㄴㅇㅁ</p>',
-      code: 'function test(){\n    console.log(1)\n}\n',
-      tag: ['tag1', 'tag2'],
-      category: '디버깅',
-      date: '2020-03-14',
-    },
-  ];
-
-  return new Promise((resolve) => resolve(dummyData[id]));
+async function myTest() {
+  try {
+    const result: any[] = [];
+    const q = await query(collection(db, 'posts'));
+    const posts = await getDocs(q);
+    posts.forEach((doc) => result.push({ id: 0, ...doc.data() }));
+    return result[0];
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
 }
 
 const Detail = ({ title, content, tag, category, date, comment }: Post) => {
   const { id } = useParams();
+  const [postData, setPostData] = useState<any>({
+    author: '1',
+    category: '1',
+    code: '1',
+    content: '1',
+    date: '1',
+    tag: ['1'],
+    title: '1',
+  });
+
+  useEffect(() => {
+    (async () => {
+      const data = await myTest();
+      console.log(data);
+      setPostData({
+        author: data.author,
+        category: data.category,
+        code: data.code,
+        content: data.content,
+        date: '2020-05-12',
+        tag: [data.tag],
+        title: data.title,
+      });
+    })();
+  }, []);
 
   return (
     <Style.Wrapper>
@@ -42,23 +58,23 @@ const Detail = ({ title, content, tag, category, date, comment }: Post) => {
         </Style.Profile>
         <Box width="16px" />
         <FlexColumnCenter>
-          <Style.Name>name</Style.Name>
+          <Style.Name>{postData.author}</Style.Name>
           <Box height="8px"></Box>
-          <Style.Created>{date}</Style.Created>
+          <Style.Created>{postData.date}</Style.Created>
         </FlexColumnCenter>
       </FlexBox>
       <Box height="20px"></Box>
-      <Style.Title>{title}</Style.Title>
+      <Style.Title>{postData.title}</Style.Title>
       <Box height="20px"></Box>
       <FlexBox gap={10}>
-        {tag?.map((t) => (
-          <Style.Label>{t}</Style.Label>
+        {postData.tag.map((t: string, idx: number) => (
+          <Style.Label key={idx}>{t}</Style.Label>
         ))}
       </FlexBox>
       <Box height="10px"></Box>
       <Divider />
       <Box height="10px"></Box>
-      <Style.Content>{content}</Style.Content>
+      <Style.Content>{postData.content}</Style.Content>
       <Box height="20px"></Box>
       <Style.CodeBlockContainer>
         <CodeBlock></CodeBlock>
