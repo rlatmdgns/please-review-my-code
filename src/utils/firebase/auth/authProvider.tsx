1b3cd4bf0baea8c40f34976e6760cc4e6e391984
설아아빠 <dhrod0325@naver.com>
2022-06-19 21:03:57 +0900

    feature : 유저 정보 동기화

--- src/utils/firebase/auth/authProvider.tsx
+++ src/utils/firebase/auth/authProvider.tsx
@@ -2,6 +2,8 @@ import { User } from 'firebase/auth';
 import { ReactNode, useEffect, useState } from 'react';
 import { auth } from './auth';
 import { AuthContext } from './authContext';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../db/db';
 
 type Props = {
   children: ReactNode;
@@ -11,8 +13,18 @@ const AuthProvider = (props: Props) => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    return auth.onAuthStateChanged((authUser) => {
-      console.log(`authUser`, authUser);
+    return auth.onAuthStateChanged(async (authUser) => {
+      if (authUser) {
+        const { uid, displayName, email } = authUser;
+        const q = await query(collection(db, 'users'), where('uid', '==', authUser?.uid));
+
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.size === 0) {
+          await addDoc(collection(db, 'users'), { uid, displayName, email });
+        }
+      }
+
       setUser(authUser);
     });
   }, []);
