a633210cb1f1b0607315b127f2c33f22ce42fac3
김승훈 <rlatmdgns94@naver.com>
2022-06-19 22:30:28 +0900

    fix: 병합 오류 해결

--- src/domain/CreateReview/index.tsx
+++ src/domain/CreateReview/index.tsx
@@ -3,11 +3,7 @@ import { TitleInput } from './TitleInput';
 import { Editor } from './Editor';
 import { Layout } from '../../components/common/Layout';
 import { OnChangeType } from './Editor/Editor';
-
 import { Editor as ToastUI } from './Editor';
-// import { TagInput } from './TagInput';
-import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
-import { Layout } from 'components/common/Layout';
 
 const CreateReview = () => {
   function handleEditorChanged(editorContent: OnChangeType) {
@@ -20,9 +16,6 @@ const CreateReview = () => {
       <Tag />
       <Editor onChange={handleEditorChanged} />
       <ToastUI />
-      {/* <TagInput /> */}
-      {/* <ContentInput /> */}
-      <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />
     </Layout>
   );
 };
