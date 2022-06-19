230f48c124e34ca48e6c2c4b6acf62d81481e6ca
설아아빠 <dhrod0325@naver.com>
2022-06-19 15:19:43 +0900

    Create review editor (#8)
    * feature : content editor
    
    * feature : content editor

--- src/domain/CreateReview/Editor/Editor.tsx
+++ src/domain/CreateReview/Editor/Editor.tsx
@@ -1,7 +1,101 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
-const Editor = () => {
-  return <div></div>;
+import { Editor as ToastEditor } from '@toast-ui/react-editor';
+
+import MonacoEditor from '@monaco-editor/react';
+import styled from 'styled-components';
+import ToastuiEditor from '@toast-ui/editor';
+
+type OnSaveType = {
+  contentHtml: string;
+  contentMarkdown: string;
+  code: string;
 };
+
+type Props = {
+  onSave?: (editor: OnSaveType) => void;
+};
+
+type MonacoEditorType = {
+  getValue: () => string;
+};
+
+const Editor = (props: Props) => {
+  const toastUiRef: any = useRef(null);
+  const [monacoEditor, setMonacoEditor] = useState({} as MonacoEditorType);
+
+  function handleOnSave() {
+    const editor = toastUiRef.current.getInstance() as ToastuiEditor;
+
+    const contentHtml = editor.getHTML();
+    const contentMarkdown = editor.getMarkdown();
+    const code = monacoEditor?.getValue();
+
+    const args = {
+      contentHtml,
+      contentMarkdown,
+      code,
+    };
+
+    console.log(args);
+
+    props.onSave?.(args);
+  }
+
+  function handleMonacoMount(editor: MonacoEditorType) {
+    setMonacoEditor(editor);
+  }
+
+  return (
+    <Wrapper>
+      <InputGroup>
+        <Label>질문</Label>
+
+        <EditorWrapper>
+          <ToastEditor
+            initialValue="hello react editor world!"
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="markdown"
+            useCommandShortcut={true}
+            ref={toastUiRef}
+          />
+        </EditorWrapper>
+      </InputGroup>
+
+      <InputGroup>
+        <Label>코드</Label>
+        <MonacoEditor
+          onMount={handleMonacoMount}
+          height="20vh"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+        />
+      </InputGroup>
+
+      <button onClick={handleOnSave}>test</button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 2px 20px;
+`;
+
+const Label = styled.label`
+  min-width: 100px;
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  background: #fff;
+  gap: 20px;
+  padding: 10px;
+`;
+
+const EditorWrapper = styled.div`
+  flex-grow: 1;
+`;
 
 export default Editor;
